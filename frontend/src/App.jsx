81d094e333cc0461775f349f51f6b387925f9dc5
Lana Miranda <lanamiranda112@gmail.com>
2025-11-04 12:24:43 -0300

    Reenviando arquivos do front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Tela_inicial from './Pages/Tela_inicial/Tela_inicial'
+import {Navbar} from './Components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      {/* Tela inicial na raiz */}
+      <Route path="/" element={<Tela_inicial />} />
+      <Route path="/navbar" element={<Navbar/>}/>
+    </Routes>
   )
 }
 
